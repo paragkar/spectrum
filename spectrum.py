82e45001ecd87ff6983ad71e8364b5283ca87f14
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-15 18:51:34 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -11,42 +11,104 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
+operators = {700: {'Vacant':0,'Railways':1,'Govt':2,'RJIO':3,'BSNL':4},
+             800: {'Vacant':0,'RCOM':1,'Govt':2,'RJIO':3,'Bharti':4, 'MTS':5, 'BSNL':6},
+             900:{'Vacant':0,'RCOM':1,'Govt':2,'Railways':3,'Bharti':4, 'AircelU':5, 
+                  'BSNLU':6,'MTNLU':7,'BhartiU':8,'VI':9,'VIU':10},
+             1800: {'Vacant':0,'RCOM':1,'Govt':2,'RJIO':3,'Bharti':4,
+                    'BhartiU':5, 'AircelR':6, 'BSNL':7,'MTNL':8,'VI':9,'VIU':10,'AircelU':11, 'Aircel':12},
+             2100: {'Vacant':0,'RCOM':1,'Govt':2,'Bharti':3, 'BSNL':4,'MTNL':5,'VI':6, 'Aircel':7},
+             2300: {'Vacant':0,'RJIO':1,'Govt':2,'Bharti':3, 'VI':4},
+             2500: {'Vacant':0,'Govt':1,'BSNL':2, 'VI':3},
+             3500: {'Vacant':0,'Bharti':1,'RJIO':2,'BSNL':3, 'MTNL':4,'VI':5},
+             26000: {'Vacant':0,'Bharti':1,'RJIO':2,'BSNL':3, 'MTNL':4,'VI':5,'Adani':6}
+            }
+
+#preparing color scale for heatmap which chnages in steps (discrete)
+def stepcolscale(operators, colcodes):
+    scale = [round(x/(len(operators)),2) for x in range(len(operators)+1)]
+    colors =[]
+    for k, v  in operators.items():
+        colors.append(colcodes.loc[k,:].values[0])
+
+    col= pd.concat([pd.DataFrame(scale),pd.DataFrame(colors)], axis=1)
+
+    col.columns =["colscale", "colors"]
+    col["colscaleshift"] = col.iloc[:,0].shift(-1)
+    # col = col.fillna(1)
+    lst=[]
+
+    for line in col.values:
+        lst.append((line[0],line[1])),
+        lst.append((line[2],line[1])),
+    lst = lst[:-2]
+    return lst
+
+
 xl = pd.ExcelFile('spectrum_map.xlsx')
 sheet = xl.sheet_names
 df = pd.read_excel('spectrum_map.xlsx', sheet_name=sheet)
 
-price = df["Master_Price_Sheet"]
+colcodes = df["ColorCodes"]
+colcodes=colcodes.set_index("Description")
 
 
 # st.sidebar.title('Navigation')
 
-# x_axis_val = st.selectbox('Select X-Axis Value', options = price.columns)
-# y_axis_val = st.selectbox('Select Y-Axis Value', options = price.columns)
+Feature = st.sidebar.selectbox('Select a Feature', options = ["Map","Price"])
 
-Type = st.sidebar.selectbox('Select Price Type', options = ["FP","DP"])
+if Feature == "Price":
+	Type = st.sidebar.selectbox('Select Price Type', options = ["FP","DP"])
+	Band = st.sidebar.selectbox('Select a Band', sorted(list(set(price["Band"]))))
+	price = df["Master_Price_Sheet"]
+	price = price[(price["Band"]==Band) & (price["Year"] != 2018)]
+	price["Year"] = sorted([str(x) for x in price["Year"].values])
 
-Band = st.sidebar.selectbox('Select a Band', sorted(list(set(price["Band"]))))
+	data = [go.Heatmap(
+			z = round(price[Type],1),
+			y = price["LSA"],
+			x = price["Year"],
+			xgap = 1,
+			ygap = 1,
+			hoverinfo ='text',
+			# text = hovertext1,
+			colorscale='Hot',
+				texttemplate="%{z}", 
+				textfont={"size":8},
+				reversescale=True,
+				),
 
-price = price[(price["Band"]==Band) & (price["Year"] != 2018)]
+	       ]
 
-price["Year"] = sorted([str(x) for x in price["Year"].values])
+if Feature == "Map":
+	Band = st.sidebar.selectbox('Select a Band', sorted(list(set(price["Band"]))))
+	freqtab = str(Band)+"MHz"
+	sf = df[freqtab]
+	sf = sf.set_index("LSA")
+	operators =operators[Band]
+	sf[sf.columns] = sf[sf.columns].replace(operators)
+	colorscalestep = stepcolscale(operators, colcodes)
+	
+	data = [go.Heatmap(
+              z = sf.values,
+              y = sf.index,
+              x = sf.columns,
+              xgap = 1,
+              ygap = 1,
+#               hoverinfo ='text',
+#               text = hovertext1,
+              colorscale=colorscalestep,
+              colorbar=dict(
+              tickvals = list(operators.values()),
+              # tickvals = tickval,
+              ticktext = list(operators.keys()),
+              dtick=1,
+              tickmode="array"),
+                ),
 
-
-data = [go.Heatmap(
-              	z = round(price[Type],1),
-              	y = price["LSA"],
-              	x = price["Year"],
-              	xgap = 1,
-            	ygap = 1,
-            	hoverinfo ='text',
-            	# text = hovertext1,
-            	colorscale='Hot',
-           		texttemplate="%{z}", 
-    			textfont={"size":8},
-    			reversescale=True,
-                	),
-
-       ]
+       		]
+	
+	
 
 fig = go.Figure(data=data )
 
