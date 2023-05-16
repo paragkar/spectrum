3016a25a8fc459446bfc379c967b02c1867564e6
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-16 10:41:20 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -105,11 +105,6 @@ if Feature == "Map":
 	operators_mapping =operators[Band]
 	st.write(operators_mapping)
 	sf[sf.columns] = sf[sf.columns].replace(operators_mapping)
-# 	colorscalestep = stepcolscale(operators_mapping, colcodes)
-# 	st.empty()
-# 	st.write(colcodes)
-# 	st.write(sf)
-# 	st.write(colorscalestep)
 	tickangle = -90
 
 	data1 = [go.Heatmap(
