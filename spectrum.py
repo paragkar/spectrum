3a06baf7ffcf55556ca0330b007f3267c0f71ca6
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-16 15:48:38 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -439,58 +439,56 @@ ayear = df_final.pivot_table(index=["LSA"], columns='StartFreq', values="Year", 
 def hovertext1(sf,ExpTab,ef,ayear,ap,rp,pf,ChannelSize,xaxisadj):  
 	hovertext = []
 	for yi, yy in enumerate(sf.index):
-	    hovertext.append([])
-	    for xi, xx in enumerate(sf.columns):
-		if ExpTab[Band]==1:
-		    expiry = round(ef.values[yi][xi],2)
-		else:
-		    expiry = "NA"
+		hovertext.append([])
+		for xi, xx in enumerate(sf.columns):
+			if ExpTab[Band]==1:
+			    expiry = round(ef.values[yi][xi],2)
+			else:
+			    expiry = "NA"
+			try:
+			    auction_year = round(ayear.loc[yy,round(xx-xaxisadj[Band],3)])
+			except:
+			    auction_year ="NA"
 
+			try:
+			    auction_price = round(ap.loc[yy,round(xx-xaxisadj[Band],3)],2)
+			except:
+			    auction_price ="NA"
 
-		try:
-		    auction_year = round(ayear.loc[yy,round(xx-xaxisadj[Band],3)])
-		except:
-		    auction_year ="NA"
+			try:
+			    reserve_price = round(rp.loc[yy,round(xx-xaxisadj[Band],3)],2)
+			except:
+			    reserve_price ="NA"
 
-		try:
-		    auction_price = round(ap.loc[yy,round(xx-xaxisadj[Band],3)],2)
-		except:
-		    auction_price ="NA"
+			if round(pf.values[yi][0],1)!=0:
+			    latest_rp = round(pf.values[yi][0],2)
+			else:
+			    latest_rp ="NA"
 
-		try:
-		    reserve_price = round(rp.loc[yy,round(xx-xaxisadj[Band],3)],2)
-		except:
-		    reserve_price ="NA"
+			operator = [k for k, v in operators.items() if v == sf.values[yi][xi]]
+			operatorold = of.values[yi][xi]
+			bandwidth = [list(val).count(operators.get(operator[0]))*ChannelSize[Band] for val in sf.values]
+			hovertext[-1].append(
+					    'Start/End Freq : {}/{} MHz (Ch)\
+					    <br />SP New/Old: {} / {} ({})\
+					    <br />BW/Expiry: {} MHz / {} Years\
+					    <br />RP/AP - Rs {} / {} Cr/MHz ({})\
+					    <br />Latest RP - Rs {} Cr/MHz'
 
-		if round(pf.values[yi][0],1)!=0:
-		    latest_rp = round(pf.values[yi][0],2)
-		else:
-		    latest_rp ="NA"
-
-		operator = [k for k, v in operators.items() if v == sf.values[yi][xi]]
-		operatorold = of.values[yi][xi]
-		bandwidth = [list(val).count(operators.get(operator[0]))*ChannelSize[Band] for val in sf.values]
-		hovertext[-1].append(
-				    'Start/End Freq : {}/{} MHz (Ch)\
-				    <br />SP New/Old: {} / {} ({})\
-				    <br />BW/Expiry: {} MHz / {} Years\
-				    <br />RP/AP - Rs {} / {} Cr/MHz ({})\
-				    <br />Latest RP - Rs {} Cr/MHz'
-
-			     .format(
-				    round(xx-xaxisadj[Band],2), 
-				    round(xx+ChannelSize[Band]-xaxisadj[Band],2),
-				    operator[0],
-				    operatorold,
-				    state_dict.get(yy), 
-				    round(bandwidth[yi],1),
-				    expiry,
-				    reserve_price,
-				    auction_price,
-				    auction_year,
-				    latest_rp,
-				    )
-				    )
+				     .format(
+					    round(xx-xaxisadj[Band],2), 
+					    round(xx+ChannelSize[Band]-xaxisadj[Band],2),
+					    operator[0],
+					    operatorold,
+					    state_dict.get(yy), 
+					    round(bandwidth[yi],1),
+					    expiry,
+					    reserve_price,
+					    auction_price,
+					    auction_year,
+					    latest_rp,
+					    )
+					    )
 	return hovertext
 
 
