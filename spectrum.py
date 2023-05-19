74a3e51498b815717602061b55d29350912db0ba
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-19 12:53:45 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -148,12 +148,13 @@ def processdff(dff):
 #convert columns of dataframe into string
 def coltostr(df):
     
-    lst =[]
-    for col in df.columns:
-        lst.append(str(col))
-    df.columns=lst
-    
-    return df
+	lst =[]
+	cols = sorted(df.columns)
+	for col in cols:
+	lst.append(str(col))
+	df.columns=lst
+
+	return df
 
 #add dummy columns for auction failed years
 
