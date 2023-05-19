fe5e400f769289b4a3ab1c4df23a76a48bf9b011
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-19 11:11:51 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -660,7 +660,7 @@ if Feature == "AuctionMap":
 		]
 	fig = go.Figure(data=data3)
 	hoverlabel_bgcolor3 = hovermatrixdata3(dff,reserveprice,auctionprice)
-	fig.update_traces(hoverlabel=dict(bgcolor=hoverlabel_bgcolor3,font=dict(size=12, color='white')))
+	fig.update_traces(hoverlabel=dict(bgcolor=list(hoverlabel_bgcolor3),font=dict(size=12, color='white')))
 	
 
 #updating figure layouts
