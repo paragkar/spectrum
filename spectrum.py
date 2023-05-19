8fb8fae0f977e6aaccc1c33bdf37a541a172ce02
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-19 13:27:37 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -642,7 +642,7 @@ if Feature == "ExpiryMap":
 	hcolscale=hcolscalefreqexp(operators, colcodes)  #colorscale for hoverbox
 	hoverlabel_bgcolor = hcolmatrixfreqexp(hcolscale, hf) #shaping the hfcolorscale
 
-type_dict ={"Auction Price": auctionprice, "Reserve Price": reserveprice, "Spectrum Offered": offeredspectrum, "Spectrum Sold": soldspectrum, "Spectrum Unsold": unsoldspectrum}
+type_dict ={"Auction Price": auctionprice, "Reserve Price": reserveprice, "Quantum Offered": offeredspectrum, "Quantum Sold": soldspectrum, "Quantum Unsold": unsoldspectrum}
 if Feature == "AuctionMap":
 	pricemaster = pricemaster[(pricemaster["Band"]==Band) & (pricemaster["Year"] != 2018)]
 	pricemaster["Year"] = sorted([str(x) for x in pricemaster["Year"].values])
