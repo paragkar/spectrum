cc46735d582bd414f489643f0f5bceb5a07435a2
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-20 06:43:39 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -229,7 +229,7 @@ if Dimension == "Calendar Year":
 	feature_list = ["Reserve Price",  "Auction Price", "Spectrum Offered", "Spectrum Sold", "Spectrum Unsold", "Block Size", "Total EMD"]
 	Feature = st.sidebar.selectbox('Select a Feature', options = feature_list)
 	z = df1[feature_dict[Feature]].values
-	st.write(df1)
+	st.write(df1["Band"])
 	
 	
 	data = [go.Heatmap(
