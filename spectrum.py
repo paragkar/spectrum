40d778abd696c8c53119eb02a5a3a9eac0e9fb45
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-20 12:17:35 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -590,7 +590,7 @@ if Dimension == "Frequency Band":
 			summarydf = eff.sum().sort_index(ascending = True).reset_index()
 			summarydf.columns = ["ExpYears", "TotalMHz"]
 			summarydf["ExpYears"]= summarydf["ExpYears"].astype(float)
-			st.bar_chart(summarydf, x = "ExpYears", y ="TotalMHz", width=1080, height=200, use_container_width =False)
+			st.bar_chart(summarydf, x = "ExpYears", y ="TotalMHz", width=1090, height=200, use_container_width =False)
 		
 			
 	
