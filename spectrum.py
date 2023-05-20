4c9d6ae0f9392d4ea4798ee83fec8b5c73f924cc
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-20 15:19:29 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -610,7 +610,7 @@ if Dimension == "Frequency Band":
 			
 # 			st.bar_chart(summarydf, x = "ExpYears", y ="TotalMHz", width=1090, height=150, use_container_width =False)	
 	
-			figsum = px.bar(summarydf, y = "TotalMHz", x = "ExpYears", width = 1100, height =200)
+			figsum = px.bar(summarydf, y = "TotalMHz", x = "ExpYears", width = 1100, height =200).update_traces(width = 0.7)
 			figsum.update_xaxes(categoryorder='array', categoryarray= sorted(list(summarydf["ExpYears"])))
 			st.plotly_chart(figsum, use_container_width=False)
 # 			st.write(figsum)
