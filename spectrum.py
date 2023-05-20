9f43a7f1b9a32c9e1632dffb075a6c1e34b940c3
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-20 08:54:33 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -675,9 +675,9 @@ if Dimension == "Frequency Band":
 	fig.update_traces(hoverlabel=dict(bgcolor=hoverlabel_bgcolor,font=dict(size=12, color='white')))
 	title = "<b>"+"Spectrum "+subtitle+" for "+str(Band)+" MHz Band "+title_map[Band]+" ("+unit+")"+"<b>"
 	if BandType[Band] == "FDD":
-		title_x =0.05
-	else:
-		title_x = 0.15
+		title_x =0.07
+	if BandType[Band] == "TDD":
+		title_x = 0.10
 
 if Dimension == "Calendar Year":
 	title = "<b>"+str(YearDim)+" - Band Wise Trend of "+Feature+" ("+units_dict[Feature]+")"+"<b>"
