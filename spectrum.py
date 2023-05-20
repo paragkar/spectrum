3f79bede0ab80d11ba60952d6dedc096e9ad9443
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-20 08:53:08 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -675,7 +675,7 @@ if Dimension == "Frequency Band":
 	fig.update_traces(hoverlabel=dict(bgcolor=hoverlabel_bgcolor,font=dict(size=12, color='white')))
 	title = "<b>"+"Spectrum "+subtitle+" for "+str(Band)+" MHz Band "+title_map[Band]+" ("+unit+")"+"<b>"
 	if BandType[Band] == "FDD":
-		title_x =0.09
+		title_x =0.05
 	else:
 		title_x = 0.15
 
