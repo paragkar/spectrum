d2eba50ffdde4de953fa92b55c8f7c227087e947
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-20 12:51:12 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -590,6 +590,7 @@ if Dimension == "Frequency Band":
 			summarydf = eff.sum().sort_index(ascending = True).reset_index()
 			summarydf.columns = ["ExpYears", "TotalMHz"]
 			summarydf["ExpYears"]= summarydf["ExpYears"].astype(float)
+			st.header("Summary of Total of All of HeatMap Below")
 			st.bar_chart(summarydf, x = "ExpYears", y ="TotalMHz", width=1090, height=200, use_container_width =False)
 		
 			
