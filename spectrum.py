3f42f3a16054b6da397182aafad8c94f9af7ba53
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-20 14:20:08 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -606,9 +606,9 @@ if Dimension == "Frequency Band":
 			summarydf.columns = ["ExpYears", "TotalMHz"]
 			summarydf["ExpYears"]= summarydf["ExpYears"].astype(float)
 		
-# 			st.bar_chart(summarydf, x = "ExpYears", y ="TotalMHz", width=1090, height=200, use_container_width =False)
+			st.bar_chart(summarydf, x = "ExpYears", y ="TotalMHz", width=1090, height=200, use_container_width =False)
 
-			st.bar_chart(summarydf, width=1090, height=200, use_container_width =False)
+
 
 			
 			data = [go.Heatmap(
