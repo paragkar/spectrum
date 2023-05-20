564335ba454f5d2bcd696a03e0de37ba202058cd
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-20 13:51:27 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -335,7 +335,7 @@ def hovertext3(dff,reserveprice,auctionprice,offeredspectrum,soldspectrum,unsold
 		hovertext.append([])
 		for xi, xx in enumerate(dff.columns):
 			winners = dff.values[yi][xi][:-2] #removing comma in the end
-			resprice = reserveprice.values[yi][xi]
+			resprice = reserveprice.loc[yi, xi]
 			st.write(reserveprice)
 			aucprice = auctionprice.values[yi][xi]
 			offmhz = offeredspectrum.values[yi][xi]
