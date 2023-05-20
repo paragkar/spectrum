d27f602ff57e109922b00bd47f69aabc009371c2
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-20 15:35:18 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -618,8 +618,8 @@ if Dimension == "Frequency Band":
 # 			figsum.update_yaxes(title='y', visible=False, showticklabels=False)
 # 			figsum.update_xaxes(categoryorder='array', categoryarray= sorted(list(summarydf["ExpYears"])))
 			
-			figsum.update_xaxes(fixedrange=True,showline=True,linewidth=1.2,linecolor='black', mirror=True)
-			figsum.update_yaxes(fixedrange=True,showline=True, linewidth=1.2, linecolor='black', mirror=True)
+# 			figsum.update_xaxes(fixedrange=True,showline=True,linewidth=1.2,linecolor='black', mirror=True)
+# 			figsum.update_yaxes(fixedrange=True,showline=True, linewidth=1.2, linecolor='black', mirror=True)
 			st.plotly_chart(figsum, use_container_width=False)
 # 			st.write(figsum)
 			
