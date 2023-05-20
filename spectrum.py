62095de743589afcad668d337f949faca1adb0da
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-20 19:03:04 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -650,8 +650,8 @@ if Dimension == "Frequency Band":
 # 		st.bar_chart(summarydf, x = "Years", y ="Total", width=1090, height=150, use_container_width =False)
 
 		bar_chart = alt.Chart(summarydf).mark_bar().encode(
-        	alt.Y('Total:Q').axis(labels=False),
-        	alt.X('Years:O').axis(lables=False),
+        	y = alt.Y('Total:Q', axis=alt.Axis(labels=False)),
+        	x = alt.X('Years:O', axis=alt.Axis(labels=False)),
 		color = alt.Color("Years:N", legend=alt.Legend(title=None, orient="right"))).properties(width=1150, height =200, title = "Summary")
 		st.altair_chart(bar_chart, use_container_width=False)
  		
