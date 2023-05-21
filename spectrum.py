e7248d8c3ce940cbf80534ee1aa9bc6128fe9f7f
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-21 08:22:52 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -734,7 +734,7 @@ if Dimension == "Frequency Band":
 	if Feature == "FreqMap":
 		unit = "Ch Size - "+str(ChannelSize[Band])+" MHz"
 	if BandType[Band] == "FDD":
-		title_x =0.12
+		title_x =0.14
 	if BandType[Band] == "TDD":
 		title_x = 0.08
 	fig.update_traces(hoverlabel=dict(bgcolor=hoverlabel_bgcolor,font=dict(size=12, color='white')))
