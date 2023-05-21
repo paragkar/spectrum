9ec4f61ef0d8ed4bd39bd66a0b181d90816cd92b
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-21 07:52:20 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -655,8 +655,8 @@ if Dimension == "Frequency Band":
 		text = bar.mark_text(dx=0, dy=-5, color = 'white' # Nudges text to right so it doesn't appear on top of the bar
 	    		).encode(text='India Total:Q')
 		
-		chart = (bar + text).properties(width=1170, height =150, title = "Summary")
-		chart = chart.configure_title(fontSize = 20, font ='Courier', anchor = 'middle', color ='black')
+		chart = (bar + text).properties(width=1170, height =150, title = "Pan India Yearly Total of the Chart Below")
+		chart = chart.configure_title(fontSize = 24, font ='Arial', anchor = 'middle', color ='black')
 		st.altair_chart(chart, use_container_width=False)
 		
 		#setting the data of the heatmap 
