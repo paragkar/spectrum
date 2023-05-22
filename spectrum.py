464f59fe10b711e79c7cf538456ef6ba715a86ab
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-22 11:15:41 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -734,7 +734,7 @@ if Dimension == "Calendar Year":
 	if Feature == "Total Outflow":
 		df1 = df1.reset_index()
 		df1_temp2 = df1.set_index(["Band","Circle"])
-		operatorslist = sorted(df1_temp2.columns[12:-1])
+		operatorslist = sorted(list(df1_temp2.columns[12:-1]))
 		selected_operators = st.sidebar.multiselect('Select Operators', operatorslist)
 		if selected_operators== []
 			df1_temp2["Total Outflow"] = df1_temp2[feature_dict["Auction Price"]]*df1_temp2["Total Sold (MHz)"]
