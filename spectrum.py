6c9271816f748fdddca4e90970011e4105f70298
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-22 08:33:07 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -866,7 +866,7 @@ fig.update_yaxes(fixedrange=True,showline=True, linewidth=1.2, linecolor='black'
 
 with st.container():
 	#plotting the main chart
-	st.text(title)
+	st.title(title)
 	st.write(fig)
 	#plotting the summary chart
 	if flag ==True:
