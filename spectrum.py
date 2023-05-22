aa1a657e3f69f338fd71c1bd0f7e2b594678ec77
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-22 19:36:15 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -850,7 +850,7 @@ if Dimension == "Calendar Year":
 			df2_temp1.drop("Band", inplace = True, axis =1)
 			df2_temp1 = df2_temp1.groupby(["Circle"]).sum().round(1)
 			df2_temp1 = df2_temp1.set_index("Circle")
-						       		       
+			st.write(df2_temp1)			       		       
 			z = df2_temp1.values
 			x = df2_temp1.columns
 			y = df2_temp1.index
