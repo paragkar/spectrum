feef2463bcc0c0400c57a759d0a18e20df693706
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-23 09:40:59 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -906,7 +906,7 @@ if Dimension == "Calendar Year":
 
 units_dict = {"Reserve Price" : "Rs Cr/MHz", "Auction Price" : "Rs Cr/MHz", "Quantum Offered": "MHz", 
 	      "Quantum Sold" : "MHz", "Quantum Unsold" : "MHz", "Total EMD" : "Rs Cr", "Total Outflow" : "Rs Cr",
-	     "Auction/Reserve" : "Ratio", "Percent Unsold" : "% of Total Spectrum", "Percent Sold" : "% of Total Spectrum", "Total Purchase" : "Rs Cr"}
+	     "Auction/Reserve" : "Ratio", "Percent Unsold" : "% of Total Spectrum", "Percent Sold" : "% of Total Spectrum", "Total Purchase" : "MHz"}
 
 #Plotting the final Heatmap	
 fig = go.Figure(data=data)
