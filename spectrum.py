284786c2462c84ddef7dba1344cddb6089ec0eea
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-23 14:45:55 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -957,7 +957,7 @@ if Dimension == "Calendar Year":
 		  ygap = 1,
 		  hoverinfo ='text',
 # 		  text = hovertext,
-		  hovertemplate = 'LSA: %{y:$.2f}<extra></extra>'
+		  hovertemplate = 'LSA: %{y:$.2f}<extra></extra>',
 		  colorscale = 'Hot',
 		    texttemplate="%{z}", 
 		    textfont={"size":10},
