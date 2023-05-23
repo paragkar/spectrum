ad274e2558941309f5db80022d23b1c21b9e2b49
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-23 08:40:55 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -889,7 +889,7 @@ if Dimension == "Frequency Band":
 		unit = units_dict[Type]
 		selected_operators = ["NA"]
 		
-		subtitle = "Unit - "+unit+"; Selected Operators - "+', '.join(selected_operators)+ " ; India Total - Sum of all Circles"
+		subtitle = "Unit - "+unit+"; Selected Operators - "+', '.join(selected_operators)+ " ; India Total - Sum of all LSAs"
 	if (Feature == "ExpiryMap") and (SubFeature == "Freq Wise"):
 		unit = "Ch Size - "+str(ChannelSize[Band])+" MHz"
 		if selected_operators == []:
