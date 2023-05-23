2e9d2a93945111dcc174a376623b4b2b085a3052
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-23 09:18:22 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -950,8 +950,8 @@ if (Dimension == "Calendar Year") and (Feature == "Operator Wise"):
 	else:
 		selectedbands = ["NA"]
 	selectedbands = [str(x) for x in selectedbands]	
-	title = "Operator Wise Yearly Trend for the Year "+str(Year)
-	subtitle = SubFeature + "; Unit -"+units_dict[SubFeature]+"; Selected Bands -" + ', '.join(selectedbands) + "; Summary - Sum of all LSAs"
+	title = "Operator Wise Summary for the Year "+str(Year)
+	subtitle = SubFeature + "; Unit -"+units_dict[SubFeature]+"; Selected Bands -" + ', '.join(selectedbands) + "; India Summary - Sum of all LSAs"
 	title_x =0.25
 	tickangle =0
 	dtickval =1
