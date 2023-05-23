2d610c1adc7a2076df50239323c9d139db67baf5
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-23 15:53:47 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -410,6 +410,7 @@ def hovertext3(dff,reserveprice,auctionprice,offeredspectrum,soldspectrum,unsold
 
 #debug
 def hovertextcal1(df1):
+	st.write(df1)
 	df1 = df1[["Circle", "Band", subfeature_dict["Auction Price"], subfeature_dict["Reserve Price"]]]
 	auctionprice =  df1.pivot(index="Circle", columns='Band', values=subfeature_dict["Auction Price"])
 	reserveprice =  df1.pivot(index="Circle", columns='Band', values=subfeature_dict["Reserve Price"])
