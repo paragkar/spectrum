848ef2add891f434a222ca667b01cdbccece31dd
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-25 13:04:19 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -186,7 +186,7 @@ def BWExpiring(sff,eff):
 	df = pd.DataFrame(lst)
 	df.columns = ["LSA","ExpYear", "Operators", "BW"]
 	df = df.set_index("LSA")
-	df = df.groupby(["ExpYear"])[["Operators"]].value_counts()*ChannelSize[Band]
+	df = df.groupby(["ExpYear","Operators"])[["BW"]].value_counts()*ChannelSize[Band]
 # 	df = df.groupby(["ExpYear", "LSA", "Operators"]).sum()
 	df = df.reset_index()
 	df.columns =["ExpYear","LSA", "Operators","BW"]
