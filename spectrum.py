3f5f4631edc786b78a5cf202806435392fdad0f5
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-25 14:54:47 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -954,13 +954,14 @@ if Dimension == "Spectrum Band":
 					st.write(selected_operator)
 					regexfilt = '(?!.*'+selected_operator+').*' #to replace na.npn with text embedded with names with the selected operator
 					temp = bandexpcalsheetf.replace(regexfilt, np.nan, regex = True)
-					selected_operator = selected_operator+"R" #for taking care of R attached in the last of the operator
-					regexfilt = '(?!.*'+selected_operator+').*' #to replace na.npn with text embedded with names with the selected operator
-					temp = bandexpcalsheetf.replace(regexfilt, np.nan, regex = True)
+					st.write(temp)
+# 					selected_operator = selected_operator+"R" #for taking care of R attached in the last of the operator
+# 					regexfilt = '(?!.*'+selected_operator+').*' #to replace na.npn with text embedded with names with the selected operator
+# 					temp = bandexpcalsheetf.replace(regexfilt, np.nan, regex = True)
 					
-					regexfilt = '^(?!.*'+selected_operator+').*' #to replace na.npn with text embedded with names of other than the selected operator
-					temp = bandexpcalsheetf.replace(regexfilt, np.nan, regex = True)
-					temp = temp.replace(selected_operator,'', regex = True)
+# 					regexfilt = '^(?!.*'+selected_operator+').*' #to replace na.npn with text embedded with names of other than the selected operator
+# 					temp = bandexpcalsheetf.replace(regexfilt, np.nan, regex = True)
+# 					temp = temp.replace(selected_operator,'', regex = True)
 				
 				for col in temp.columns:
 					temp[col] = temp[col].astype(float)
