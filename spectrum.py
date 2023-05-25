64fe402a60cbfbf79c39c8654c6fc9a64417850f
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-25 15:15:59 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -950,7 +950,7 @@ if Dimension == "Spectrum Band":
 					temp = bandexpcalsheetf.replace(regexfilt, np.nan, regex = True)
 					temp = temp.replace(selected_operator,'', regex = True)
 				else:
-					regexfilt = '[0-9.]+'selected_operator+'[RU]' #to replace na.npn with text ending RU with names with the selected operator
+					regexfilt = '[0-9.]+'+selected_operator+'[RU]' #to replace na.npn with text ending RU with names with the selected operator
 					temp = bandexpcalsheetf.replace(regexfilt, np.nan, regex = True)
 					st.write(temp)
 					regexfilt = '^(?!.*'+selected_operator+').*' #to replace na.npn with text embedded with names of other than the selected operator
