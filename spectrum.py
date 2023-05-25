22b68eac4295b66559a045a3369e7368c507d200
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-25 09:47:53 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -937,8 +937,7 @@ if Dimension == "Spectrum Band":
 			if selected_operator == "All":
 				hovertext = hovertext22(bwf,eff) #hovertext for "All"
 			else:
-				st.write(selected_operator)
-				regexfilt = '^(?!.*'+selected_operator+').*' #to replace na.npn with text embedded with names of other than the selected operator
+				regexfilt = '^(?!.*'+'selected_operator'+').*' #to replace na.npn with text embedded with names of other than the selected operator
 				temp = bandexpcalsheetf.replace(regexfilt, np.nan, regex = True)
 				temp = temp.replace(selected_operator,"", regex = True)
 				for col in temp.columns:
