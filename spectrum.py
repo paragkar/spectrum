be5ff69edb1535d5c5737ff7899d9515c2cf9165
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-25 15:20:06 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -952,13 +952,15 @@ if Dimension == "Spectrum Band":
 				else:
 					regexfilt = '[0-9.]+'+selected_operator+'[RU]' #to replace na.npn with text ending RU with names with the selected operator
 					temp = bandexpcalsheetf.replace(regexfilt, np.nan, regex = True)
-					st.write(temp)
+	
 					regexfilt = '^(?!.*'+selected_operator+').*' #to replace na.npn with text embedded with names of other than the selected operator
 					temp = bandexpcalsheetf.replace(regexfilt, np.nan, regex = True)
 					temp = temp.replace(selected_operator,'', regex = True)
+					st.write(temp)
+					
 				
-# 				for col in temp.columns:
-# 					temp[col] = temp[col].astype(float)
+				for col in temp.columns:
+					temp[col] = temp[col].astype(float)
 				eff = forexpyearheatmap(temp,selected_operator)
 				hovertext = hovertext23(eff) #hovertext with operator selections
 			
