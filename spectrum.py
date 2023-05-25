f4f154c147bc44f9f12ef6af6db78d2524bc668e
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-25 14:00:36 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -349,7 +349,7 @@ def hovertext21(sf,sff,ef,of,bandf,bandexpf,ExpTab,ChannelSize,xaxisadj,ayear):
 def hovertext22(bwf,eff): 
 	bwf["Op&BW"] = bwf["Operators"]+" - "+round(bwf["BW"],2).astype(str)+" MHz"
 	bwff = bwf.set_index("LSA").drop(['Operators'], axis=1)
-	st.write(bwff)
+	xaxisyears = sorted(list(set(bwff["ExpYear"])))[1:]
 	hovertext = []
 	for yi, yy in enumerate(eff.index):
 		hovertext.append([])
