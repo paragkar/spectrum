3d8a993da4a8732312f228ae555d44e0bf4917a2
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-27 18:26:42 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1586,7 +1586,7 @@ if selected_dimension == "Telecom Data":
 
 		dftelesubsprocess.drop(columns = ["Category"], axis =1, inplace = True)
 
-		dftelesubsprocess = pd.pivot(data = dftelesubsprocess, index = ["Date","Circle"], values= dftelesubsprocess.columns[2:])
+		dftelesubsprocess = pd.pivot(data = dftelesubsprocess, index = ["Date","Circle"], columns= dftelesubsprocess.columns[2:])
 
 
 		st.write(dftelesubsprocess)
