49d19bac20a636d5f6ab56e923c95c744c751e53
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-28 18:21:54 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1623,7 +1623,7 @@ if selected_dimension == "Business Data":
 
 		dftotal.columns = ["Date", "Circle", "Operator", "Subs"]
 
-		st.write(state_dict.keys()) #debug
+		st.write(state_dict.values()) #debug
 
 		dftotal = dftotal.groupby(["Date","Operator","Circle"]).sum()
 
