50b102efa5aa822fceb4bae26718b2f4872f475c
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-28 15:35:21 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1932,7 +1932,7 @@ else:
 	pass
 
 #removes tic labels if the date_range_list greater than a value
-if (selected_dimension == "Business Data") and (SubFeature == "Subscribers Trends"):
+if (selected_dimension == "Business Data") and (Feature == "Subscribers Trends"):
 	if len(date_range_list) >= 30:
 		fig.update_xaxes(
 		    tickmode='array',
