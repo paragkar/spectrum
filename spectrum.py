67ba057f7e1e5fbe88cb0807600e0229f1444a2d
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-28 08:44:31 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1613,8 +1613,10 @@ if selected_dimension == "Telecom Data":
 		slider_range = st.slider("Double ended slider", min_value = dftotal.columns[0], 
 			max_value = dftotal.columns[-1], value =(dftotal.columns[-20],dftotal.columns[-1]))
 
+		st.write(dftotal.columns[-1])
 
-		dftotal = dftotal.sort_values(slider_range[0], ascending = False) #filter the data on the first column selected by slider
+
+		# dftotal = dftotal.sort_values(slider_range[0], ascending = False) #filter the data on the first column selected by slider
 
 		st.write(slider_range)
 
