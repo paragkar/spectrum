5c3a0082ebba1f11803fa9edc3ac34cbcf8f80e6
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-28 13:31:15 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -23,6 +23,7 @@ import msoffcrypto
 
 #Setting Page layout
 st.set_page_config(layout="wide")
+st. set_page_config(initial_sidebar_state="collapsed") 
 #set summary chart flag
 flag = False # It will toggle to True when we what summary chart to show
 
