ea146d7d1d9e88adf91772b97f21bb6de8d48154
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-29 19:56:45 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2278,6 +2278,7 @@ if authentication_status: #if authentication sucessful then app is rendered
 		st.markdown(style, unsafe_allow_html=True)
 		st.header(title)
 		st.markdown(subtitle)
+		st.progress(10) #debug
 		st.plotly_chart(fig, use_container_width=True)
 		#plotting the summary chart
 
