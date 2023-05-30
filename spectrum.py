c1b1248568aa734ac9ac336421d482a29927da85
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-30 13:06:05 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2346,7 +2346,7 @@ if authentication_status: #if authentication sucessful then app is rendered
 		st.markdown(subtitle)
 		tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"]) #for listing the summary chart for freq layout
 		tab1.plotly_chart(fig, use_container_width=True)
-		tab2.dataframe(summarydf)
+		tab2.table(summarydf)
 
 		#plotting the summary chart
 
