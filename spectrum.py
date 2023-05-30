929efdfd797f1c9be0edbcc2ed974a1d88e62706
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-30 08:31:05 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1132,7 +1132,7 @@ if authentication_status: #if authentication sucessful then app is rendered
 						counts.append(count_items(row))
 
 				for i in range(len(counts)):
-					del counts[i][np.nan]
+					del counts[i]["nan"]
 
 
 				st.write(counts)
