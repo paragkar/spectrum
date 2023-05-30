6fc297771a7c682f67fc4ad86dd719452c64c694
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-30 08:00:33 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1125,7 +1125,7 @@ if authentication_status: #if authentication sucessful then app is rendered
 				for row in sf.values:
 					counts.append(count_items(row))
 
-				summarydf = pd.DataFrame(counts)*channelsize_dict[Band].dropna()
+				summarydf = pd.DataFrame(counts)*channelsize_dict[Band].dropna(axis=1)
 
 				st.write(summarydf)
 
