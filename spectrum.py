6be850f8185f985e52d5cfc8afa39a37c4856c6a
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-30 11:55:20 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1143,7 +1143,7 @@ if authentication_status: #if authentication sucessful then app is rendered
 						operatorname = get_key_from_value(operators,int(col))
 						summarydf.rename(columns = {col : operatorname}, inplace = True)
 				else:
-					summarydf.rename(columns = {col : cols_to_replace[int(col)]}, inplace = True)
+					summarydf.rename(columns = {col : selected_operators[int(col)]}, inplace = True)
 
 
 				st.write(summarydf)
