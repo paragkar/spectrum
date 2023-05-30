2b34cdc4ee2222bccaf1bd56ebd029805f78a6a1
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-30 09:22:27 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2337,7 +2337,7 @@ if authentication_status: #if authentication sucessful then app is rendered
 		st.markdown(subtitle)
 		col1,col2 = st.columns([14,1]) #for listing the summary chart for freq layout
 		if not summarydf.empty:
-			st.header("Summary")
+			col2.header("Summary")
 			col1.plotly_chart(fig, use_container_width=True)
 			col2.write(summarydf, use_container_width=True)
 		else:
