94a727bcf5003a368598d58508866e929eca2ded
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-30 11:28:40 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1134,7 +1134,7 @@ if authentication_status: #if authentication sucessful then app is rendered
 
 				summarydf.index = sf.index
 
-				st.write(operatorslist)
+				st.write(operators)
 
 
 				# for col in summarydf:
