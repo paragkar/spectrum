a00ec8873c62e23939eb7d938e3313a623cb18e5
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-05-31 16:16:16 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2181,7 +2181,7 @@ if authentication_status: #if authentication sucessful then app is rendered
 
 			dflfsfprocess = dflfsfprocess.groupby(['Category','LicenseType','FY']).sum().drop(columns=["Operators"], axis =1).reset_index()
 
-			dflfsfbylicense = dflfsfprocess.pivot(index ='LicenseType', columns ='FY', values ='Amount').sort_values("2023-2024", ascending = False)/10000000
+			dflfsfbylicense = round(dflfsfprocess.pivot(index ='LicenseType', columns ='FY', values ='Amount').sort_values("2023-2024", ascending = False)/10000000,1)
 
 			st.write(dflfsfbylicense)
 
