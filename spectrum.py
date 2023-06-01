c202c5ead5ca335341fb85f32ba10cfee3c94116
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-01 16:03:24 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2168,7 +2168,7 @@ if authentication_status: #if authentication sucessful then app is rendered
 
 			dfoperatornames = dfT["LFSF_Op_Names_Map"]
 
-			operators_dict = df.groupby(['Mapped_Names']).apply(lambda x: x['Original_Names'].tolist()).to_dict()
+			operators_dict = dfoperatornames.groupby(['Mapped_Names']).apply(lambda x: x['Original_Names'].tolist()).to_dict()
 
 			st.wtite(operators_dict)
 
