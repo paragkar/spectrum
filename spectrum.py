2cca03325a771327cbb1ce5101e72b9ab289d117
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-02 16:18:21 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2179,6 +2179,8 @@ if authentication_status:
 
 				sorted_df = dflfsfprocess.groupby(["Operators","FY"]).sum()
 
+				st.write(sorted_df)
+
 				sorted_df = sorted_df[sorted_df["FY"]=="2022-2023"].sort_values(by='Amount', ascending=False)
 
 				sorted_operators = list(set(sorted_df["Operators"]))
