45c3857bb886212776517d1b32da3af1a499e4c9
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-03 09:18:28 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1348,8 +1348,8 @@ if authentication_status:
 						temp = bandexpcalsheetf.replace(regexfilt1, np.nan, regex = True)
 						regexfilt2 = '[0-9.]+'+selected_operator+'U'  #to replace na.npn with text ending U with names with the selected operator
 						temp = bandexpcalsheetf.replace(regexfilt2, np.nan, regex = True)
-						regexfilt = '^(?!.*'+selected_operator+').*' #to replace na.npn with text embedded with names of other than the selected operator
-						temp = temp.replace(regexfilt, np.nan, regex = True)
+						# regexfilt = '^(?!.*'+selected_operator+').*' #to replace na.npn with text embedded with names of other than the selected operator
+						# temp = temp.replace(regexfilt, np.nan, regex = True)
 						temp = temp.replace(selected_operator,'', regex = True)
 					
 					
