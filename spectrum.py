c5d5902eb6b7631ee5891dcec2c23d1395d3d917
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-03 15:46:36 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -919,7 +919,7 @@ if authentication_status:
 
 		st.write(dfabsolute)
 	
-		dfpercent = round(((dflfsfbysubfeature/summarydf).head(20))*100,2)
+		dfpercent = round(((dflfsfbysubfeature/summarydf.T).head(20))*100,2)
 
 		st.write(summarydf)
 
