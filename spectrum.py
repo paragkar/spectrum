ae685a7de9633997cdecd54913889fb6d29f763f
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-04 15:16:21 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2672,7 +2672,7 @@ if authentication_status:
 			    ),
 			    yaxis2=dict(
 			        title='BTS',
-			        range=[1500000, 20000000],  # Set the range for y-axis 2
+			        range=[1500000, 25000000],  # Set the range for y-axis 2
 			        domain=[0.35, 0.62]
 			    ),
 			    yaxis3=dict(
