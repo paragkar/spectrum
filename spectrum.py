38410949ddde22b46e04fa811e45b064ad6033e7
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-04 16:15:28 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2329,9 +2329,9 @@ if authentication_status:
 			dftowersbts = dftowersbts.sort_values("Date", ascending=True)
 			dftowersbts["Ratio"] = dftowersbts["BTS"] / dftowersbts["Towers"]
 
-			trace1 = go.Scatter(x=dftowersbts.index, y=dftowersbts["Ratio"], name="BTSs/Towers", yaxis="y1", mode = 'lines+markers+text',text=list(round(dftowersbts["Ratio"]/1000000,1)),
+			trace1 = go.Scatter(x=dftowersbts.index, y=dftowersbts["Ratio"], name="BTSs/Towers", yaxis="y1", mode = 'lines+markers+text',text=list(round(dftowersbts["Ratio"],1)),
     							textposition="bottom center", showlegend = False, line = dict(color ='red'))
-			trace2 = go.Scatter(x=dftowersbts.index, y=dftowersbts["BTS"], name="BTS Trends", yaxis="y2", mode = 'lines+markers+text',text=list(round(dftowersbts["BTS"],1)),
+			trace2 = go.Scatter(x=dftowersbts.index, y=dftowersbts["BTS"], name="BTS Trends", yaxis="y2", mode = 'lines+markers+text',text=list(round(dftowersbts["BTS"]/100000,1)),
 								textposition="bottom center", showlegend = False, line = dict(color = 'green'))
 			trace3 = go.Scatter(x=dftowersbts.index, y=dftowersbts["Towers"], name="Tower Trends", yaxis="y3", mode = 'lines+markers',
 								showlegend = False, line = dict(color = 'blue'))
