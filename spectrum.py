100a864cad59eba22b4fefa91f25be1ea10c88eb
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-04 16:17:07 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2329,7 +2329,9 @@ if authentication_status:
 			dftowersbts = dftowersbts.sort_values("Date", ascending=True)
 			dftowersbts["Ratio"] = dftowersbts["BTS"] / dftowersbts["Towers"]
 
-			trace1 = go.Scatter(x=dftowersbts.index, y=dftowersbts["Ratio"], name="BTSs/Towers", yaxis="y1", mode = 'lines+markers+text',text=list(round(dftowersbts["Ratio"],1)),
+			trace1 = go.Scatter(x=dftowersbts.index, y=dftowersbts["Ratio"], name="BTSs/Towers", yaxis="y1", 
+								textfont=dict(family="sans serif",size=8,color="LightSeaGreen"),
+								mode = 'lines+markers+text',text=list(round(dftowersbts["Ratio"],1)),
     							textposition="bottom center", showlegend = False, line = dict(color ='red'))
 			trace2 = go.Scatter(x=dftowersbts.index, y=dftowersbts["BTS"], name="BTS Trends", yaxis="y2", mode = 'lines+markers+text',text=list(round(dftowersbts["BTS"]/100000,1)),
 								textposition="bottom center", showlegend = False, line = dict(color = 'green'))
