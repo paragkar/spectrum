92f849cb652dc90c8ff3d3311254301874c3b8c6
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-05 14:56:22 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1543,7 +1543,7 @@ if authentication_status:
 			    template="seaborn",
 			    # xaxis=dict(title_text="Rounds"),
 			    # yaxis=dict(title_text="Circles"),
-			  	height = 600,
+			  	height = 650,
 			)
 
 			st.plotly_chart(figauc, use_container_width=True)
