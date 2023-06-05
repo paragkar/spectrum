7f64b9f996e8ca7e94850f765a7db0d09f510966
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-05 15:23:40 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1552,7 +1552,7 @@ if authentication_status:
 			   	height = 650,)
 
 			# Update x-axis tick font for all subplots
-			figauc.update_xaxes(tickfont=dict(size=8))
+			figauc.update_xaxes(tickfont=dict(size=10))
 
 			st.plotly_chart(figauc, use_container_width=True)
 
