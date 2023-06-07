1ce8decd0e649d5b7e55999164f4330982a490d9
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-07 10:21:27 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1742,7 +1742,7 @@ if authentication_status:
 
 					# Add a dummy trace for the vertical border
 					dummy_trace = go.Scatter(
-							    x=[data['x'][0], data['x'][-1]],
+							    x=[data['x'][-1], data['x'][-1]],
 							    y=[data['y'][0], data['y'][-1]],
 							    mode='lines',
 							    line=dict(color='black', width=1))
