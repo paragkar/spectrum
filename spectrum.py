8cf336dc3071b63b23ca32755f3addded3b852a1
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-07 09:08:01 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1723,7 +1723,7 @@ if authentication_status:
 
 					dftemp = dftemp.groupby(["LSA", "Bidder", "Rank_PWB_End_ClkRd"]).sum().reset_index()
 
-					st.write(dftemp.columns) #debug
+					st.write(dftemp) #debug
 
 					data = [go.Heatmap(
 						z=dftemp["Rank_PWB_End_ClkRd"],
