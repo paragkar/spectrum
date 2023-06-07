58502697eb3e956f64d01f71ecbc6939c5ef8930
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-07 08:51:05 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1719,11 +1719,11 @@ if authentication_status:
 
 					dfbid = dfbid[filt]
 
-					st.write(dfbid.columns) #debug
-
 					dftemp = dfbid.drop(columns=["Possible_Raise_Bid_ClkRd", "Rank_PWB_Start_ClkRd","Bid_Decision","PWB_End_ClkRd"], axis=1).reset_index()
 
 					dftemp = dftemp.groupby(["LSA", "Bidder", "Rank_PWB_End_ClkRd"]).sum().reset_index()
+
+					st.write(dfbid.columns) #debug
 
 					data = [go.Heatmap(
 						z=dftemp["Rank_PWB_End_ClkRd"],
