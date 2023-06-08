068193f54b0c38f1edf65b7544d446507d2b4fe9
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-08 15:44:03 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2011,7 +2011,7 @@ if authentication_status:
 						df = dftemp.copy()
 
 						for col in df.columns:
-							st.write(col, min([x for x in df[col] if (x >0 or x !=np.nan)]))
+							st.write(col, max([x for x in df[col] if (x >0 or x !=np.nan)]))
 
 					if chartoption == "ReservePrice Multiple":
 
