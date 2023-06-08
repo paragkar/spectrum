f965dd7c464ee5cd9f268be6903cb089080d7395
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-08 17:45:53 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2098,8 +2098,6 @@ if authentication_status:
 
 					col1,col2 = st.columns([10,1]) #create collumns of uneven width
 					col1.plotly_chart(figauc, use_container_width=True)
-					col2.header("")
-					col2.markdown("")
 					col2.plotly_chart(figpanindiabids, use_container_width=True)
 
 
