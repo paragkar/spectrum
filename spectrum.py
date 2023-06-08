9b661ec4cc8a9568fafd541c7a1df026e7e20eab
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-08 14:46:36 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2012,11 +2012,11 @@ if authentication_status:
 
 						min_bids=[]
 						for col in df.columns:
-					    	min_bids.append(min([value for value in list(df[col]) if value != 0]))
+							min_bids.append(min([value for value in list(df[col]) if value != 0]))
 					    outflow=[]
 					    for i, row in enumerate(df.columns):
-					    	row = [1 for x in row if x >0]
-					    	outflow.append(sum(row * min_bids[i]))
+							row = [1 for x in row if x >0]
+							outflow.append(sum(row * min_bids[i]))
 					    st.write(outflow)
 
 					if chartoption == "ReservePrice Multiple":
