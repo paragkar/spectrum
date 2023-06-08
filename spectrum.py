4a6119cdd338d6594fe47799689fbddb7e1abdb7
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-08 08:26:36 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1631,7 +1631,7 @@ if authentication_status:
 
 					selected_lsa = [k for k, v in state_dict.items() if v == sortbylsa]
 
-					dftempheat = dftempheat.replace(np.nan, "NA")
+					# dftempheat = dftempheat.replace(np.nan, "NA")
 
 					dftempheat = dftempheat.sort_values(selected_lsa[0], ascending = False)
 
