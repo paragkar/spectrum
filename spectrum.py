d00b7687e7da824f41ff612b2f2b688a3f3875f1
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-08 13:23:26 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1645,14 +1645,14 @@ if authentication_status:
 				if radio_selection == "Absolute Values":
 
 					dftempheat = dftemp.pivot(index="Bidder", columns='LSA', values="Bid_Decision")
-					texttemplate = "%{z}",
+					texttemplate = "%{z}"
 
 					# z = dftemp["Bid_Decision"]
 
 				if radio_selection == "Percentage of Total":
 
 					dftempheat = dftemp.pivot(index="Bidder", columns='LSA', values="Bid_Decision_Perc")
-					texttemplate = "%{z}",
+					texttemplate = "%{z}"
 
 					# z = dftemp["Bid_Decision_Perc"]
 
