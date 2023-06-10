862963b63d9d4f8531553d26acae96986a36f24b
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-10 18:02:45 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2552,7 +2552,7 @@ if authentication_status:
 		        tickmode='array',
 		        	))
 
-			title = titlesubpart+" - Blocks Selected in the Round No -"+str(round_number)
+			title = titlesubpart+" - Blocks Selected at Round No -"+str(round_number)
 			subtitle = "Unit - Numbers; Block Size = "+ str(blocksize) +" MHz; Source - DoT"
 
 			style = "<style>h3 {text-align: left;}</style>"
