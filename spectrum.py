9c8f84076aeba14605ddb0a2f8539ed0bf553d37
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-10 10:06:58 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2963,7 +2963,7 @@ if authentication_status:
 						showscale=False,
 							# texttemplate="%{z}", 
 							# textfont={"size":10},
-							# reversescale=True,
+							reversescale=True,
 							)]
 						
 
