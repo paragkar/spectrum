a3b347d712584126ed7bc3d60f1ef6cccd4ab1b8
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-11 13:43:53 +0530

    New 2014 data added

--- spectrum.py
+++ spectrum.py
@@ -1919,7 +1919,41 @@ if authentication_status:
 
 	if selected_dimension == "Auction Data":
 
-		Feature = st.sidebar.selectbox("Select a Feature", ["2010-Band2100","2010-Band2300", "2012-Band1800"])
+		Feature = st.sidebar.selectbox("Select a Feature", ["2010-Band2100","2010-Band2300", "2012-Band1800","2014-Band1800","2014-Band900"])
+
+		if Feature == "2014-Band1800":
+
+			totalrounds = 68
+			mainsheet = "2014_2G_1800"
+			activitysheet = "2014_2G_Activity"
+			demandsheet = "2014_2G_1800_AD"
+			titlesubpart = "1800 MHz Auctions (CY-2014)"
+			xdtick =5
+			zmin=1 
+			zmax=5
+			zmin_af = 0.5
+			zmax_af = 1
+			texttempbiddemandactivity = ""
+			blocksize = 0.2
+			zmin_blk_sec = 0
+			zmax_blk_sec = 1
+
+		if Feature == "2014-Band900":
+
+			totalrounds = 68
+			mainsheet = "2014_2G_900"
+			activitysheet = "2014_2G_Activity"
+			demandsheet = "2014_2G_900_AD"
+			titlesubpart = "900 MHz Auctions (CY-2014)"
+			xdtick =5
+			zmin=1 
+			zmax=5
+			zmin_af = 0.5
+			zmax_af = 1
+			texttempbiddemandactivity = ""
+			blocksize = 0.2
+			zmin_blk_sec = 0
+			zmax_blk_sec = 1
 
 
 		if Feature == "2010-Band2100":
