280add5d92bda57613d96f6f1f61bbb8aebf70aa
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-11 11:05:14 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2826,7 +2826,7 @@ if authentication_status:
 
 				
 				#plotting all charts 
-				col1,col2 = st.columns([14,1]) #create collumns of uneven width
+				col1,col2 = st.columns([10,1]) #create collumns of uneven width
 				col1.plotly_chart(figauc, use_container_width=True)
 				col1.altair_chart(figsumcols, use_container_width=True)
 				col2.plotly_chart(figsumrows, use_container_width=True)
@@ -2913,7 +2913,7 @@ if authentication_status:
 				# st.plotly_chart(figauc, use_container_width=True)
 
 				#plotting all charts 
-				col1,col2 = st.columns([14,1]) #create collumns of uneven width
+				col1,col2 = st.columns([10,1]) #create collumns of uneven width
 				col1.plotly_chart(figauc, use_container_width=True)
 				col1.altair_chart(figsumcols, use_container_width=True)
 				col2.plotly_chart(figsumrows, use_container_width=True)
