93d58eb85c0a18a5d997ac63a673f9df62b72c7e
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-11 16:24:48 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2251,7 +2251,14 @@ if authentication_status:
 
 				#sort by LSA 
 
-				sortbylsa = st.sidebar.selectbox("Select a Circle to Sort", state_dict.values())
+				circle_list=[]
+
+				for circle in listofcircles: #this extracts the full name of the circle from the code
+					circle_list.append(state_dict[circle])
+
+				# sortbylsa = st.sidebar.selectbox("Select a Circle to Sort", state_dict.values())
+
+				sortbylsa = st.sidebar.selectbox("Select a Circle to Sort", circle_list)
 
 				selected_lsa = [k for k, v in state_dict.items() if v == sortbylsa]
 
