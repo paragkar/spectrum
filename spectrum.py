e7f134dc7499a3f2100f6404f66b4fd24580a4d4
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-11 06:50:37 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2367,7 +2367,7 @@ if authentication_status:
 					showscale=False,
 						texttemplate="%{z}", 
 						textfont={"size":10},
-						reversescale=True,
+						# reversescale=True,
 						)]
 
 			
@@ -2391,7 +2391,7 @@ if authentication_status:
 						showscale=True,
 							texttemplate="%{z}", 
 							textfont={"size":10},
-							reversescale=True,
+							# reversescale=True,
 							)]
 					
 
@@ -2504,7 +2504,7 @@ if authentication_status:
 					showscale=False,
 						texttemplate="%{z}", 
 						textfont={"size":10},
-						reversescale=True,
+						# reversescale=True,
 						)]
 
 				if chartoption == "ReservePrice Multiple":
@@ -2527,7 +2527,7 @@ if authentication_status:
 						showscale=True,
 							texttemplate="%{z}", 
 							textfont={"size":10},
-							reversescale=True,
+							# reversescale=True,
 							)]
 					
 
