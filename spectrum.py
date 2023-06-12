26cac958c10ffa64accbe082a5b298afabccea78
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-12 13:04:08 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -3884,13 +3884,13 @@ if authentication_status:
 				dflastsubbid = dflastsubbid.reset_index().pivot(index="Bidder", columns='LSA', 
 								values="Last_Sub_Bid_Start_CLKRd").sort_index(ascending=False)
 
-				dflastsubbid = dflastsubbid.replace(0, np.nan).dropna().sort_index()
+				# dflastsubbid = dflastsubbid.replace(0, np.nan).dropna().sort_index()
 
 				# dfrp = dflastsubbidRD2["Last_Sub_Bid_Start_CLKRd"].drop_duplicates().replace(0, np.nan).dropna().sort_index()
 
 				dfrp = dflastsubbidRD2["Last_Sub_Bid_Start_CLKRd"].reset_index().drop_duplicates().replace(0,np.nan).dropna().set_index("LSA").sort_index()
 
-				st.write(dfrp)
+				st.write(dflastsubbid/dfrp)
 
 
 
