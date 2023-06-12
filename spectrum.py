b20bcbb87410edcc3864e9108c42ad09edf3b6d6
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-12 13:00:00 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -3890,7 +3890,7 @@ if authentication_status:
 
 				# dfrp = dflastsubbidRD2["Last_Sub_Bid_Start_CLKRd"].drop_duplicates().replace(0, np.nan).dropna().sort_index()
 
-				dfrp = dflastsubbidRD2["Last_Sub_Bid_Start_CLKRd"].sort_index()
+				dfrp = dflastsubbidRD2["Last_Sub_Bid_Start_CLKRd"].reset_index().drop_duplicates()
 
 				st.write(dfrp)
 
