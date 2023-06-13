7598ee576d4aa047097a7f1cd6e2fa05ea82888b
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-13 13:53:44 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -3826,7 +3826,7 @@ if authentication_status:
 
 				blocksforsale.columns = ["LSA", "Blocks"]
 
-				figblkssale = px.bar(blocksforsale, x="Blocks", y="LSA", orientation='h', height = 600) #plotly horizontal bar chart 
+				figblkssale = px.bar(blocksforsale, x="Blocks", y="LSA", orientation='h', height = 620) #plotly horizontal bar chart 
 
 				figblkssale.update_layout(xaxis=dict(title='BlocksForSale'), yaxis=dict(title=''))
 
