34030149ad543a2717eef64510f2a1ecef5a1a92
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-13 07:20:47 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2514,6 +2514,8 @@ if authentication_status:
 
 					figpanindiabids.update_yaxes(visible=False, showticklabels=False)
 
+					figpanindiabids.update_layout(height = 615)
+
 					hovertext, colormatrix = htext_colormatrix_auctiondata_2010_3G_BWA_ProvWinningBid(dfrp, dftemp, pwbtype, round_number) #debug
 
 					data = [go.Heatmap(
