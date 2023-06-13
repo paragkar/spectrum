60043e44afb5f1c12eac2f3ec69b83ae1c2ecf67
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-13 11:44:06 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2220,6 +2220,7 @@ if authentication_status:
 
 				resultdf = resultdf[::-1] #reverse the dataframe from the index point of view
 
+				dfprovallcblks_endrd = dfprovallcblks_endrd.sort_values(selected_lsa[0], ascending = True)
 
 				st.write(dfprovallcblks_endrd)
 
