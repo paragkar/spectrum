2e8c3172812eb71cf682a02a8fdfcd1976dbaea8
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-13 18:47:15 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -4127,8 +4127,7 @@ if authentication_status:
 
 				dfrp = dflastsubbidRD2["Last_Sub_Bid_Start_CLKRd"].reset_index().drop_duplicates().set_index("LSA").sort_index()
 
-				# dfrp = dfrp.reset_index().drop_duplicates(subset="LSA", keep='last').set_index("LSA")
-
+				dfrp = dfrp.groupby('Index').filter(lambda x: not (x['Last_Sub_Bid_Start_CLKRd'] == 0).any() and len(x) == 1)
 
 				st.write(dflastsubbidheat) #debug
 
