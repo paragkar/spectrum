1a83e1f09417ecec2374e598b757fdf47cc6141e
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-18 13:46:27 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -6015,7 +6015,7 @@ if authentication_status:
 			df_rev.drop(columns=["License", "Year", "Month","Circle","Dollar Rate"], inplace = True)
 
 			
-			df_temp = df_rev.groupby(["Date","Operator"]).sum().sort_values("GR", ascending = False).round(0).sort_index(ascending = False)
+			df_temp = df_rev.groupby(["Date","Operator"]).sum().sort_index(ascending = False).sort_values("GR", ascending = False).round(0)
 
 			st.write(df_temp)
 
