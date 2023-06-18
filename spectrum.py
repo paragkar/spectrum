254d41318086a08c5e7b2ff3f61132699a435249
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-18 15:32:15 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -6039,7 +6039,7 @@ if authentication_status:
 					xgap = 1,
 					ygap = 1,
 					hoverinfo ='text',
-					text = hovertext,
+					# text = hovertext,
 					colorscale='Hot',
 						texttemplate="%{z}", 
 						textfont={"size":10},
