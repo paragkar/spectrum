4d7dc9bb2b088af9c95682e7df65bf5cb28cbf21
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-18 08:39:02 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -5979,8 +5979,8 @@ if authentication_status:
 
 			df_fin["Date"] = pd.to_datetime(df_fin["Date"])
 
-			min_date = datetime(2010,1,31).dt.strftime("%d/%m/%Y")
-			max_date = max(df_fin["Date"]).dt.strftime("%d/%m/%Y")
+			min_date = datetime(2010,1,31).date()
+			max_date = max(df_fin["Date"]).date()
 
 			st.write(min_date, max_date)
 
