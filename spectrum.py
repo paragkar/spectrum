d1dccd9bd029d5a5b4ddf9c7e1a44cafe1992f52
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-18 18:10:00 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -6021,7 +6021,7 @@ if authentication_status:
 
 			df_finmetric = df_finmetric.sort_values(df_finmetric.columns[-1], ascending = False)
 
-			df_finmetricINC = (df_finmetric - df_finmetric.shift(1, axis =1))
+			df_finmetricINC = (df_finmetric - df_finmetric.shift(1, axis =1)).head(15)
 
 			summarydf = df_finmetric.sum(axis=0)
 
