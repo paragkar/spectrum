c757431ddce31aeb50af922f4fe8faae236510cd
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-19 12:39:10 +0530

    database file deleated

--- spectrum.py
+++ spectrum.py
@@ -1459,6 +1459,28 @@ if authentication_status:
 
 	#**********  Main Program Starts here ***************
 
+	from deta import Deta
+
+
+	DETA_KEY= st.secrets["deta_auth_tele_app"]
+
+	deta = Deta(DETA_KEY)
+
+	db = deta.Base("users_db")
+
+	def insert_user(username, name, password):
+
+		#Returns the users on a successful user creation, othewise raises an error
+
+		return db.put({"key" : username, "name": name, "password" : password})
+
+	insert_user("pparker", "Peter Parker", "abc123")
+
+
+
+
+
+
 	authenticator.logout("Logout", "sidebar") #logging out authentication
 	st.sidebar.title(f"Welcome {name}")
 	# image = Image.open('parag_kar.jpg') #debug
