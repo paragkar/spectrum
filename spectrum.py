f2261dddcb708f7e435ceb20c44186a6bb4b0b85
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-19 06:46:08 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -6452,7 +6452,7 @@ if authentication_status:
 		xdtickangle =-45
 		xdtickval = 2
 		title = "Indian Telecom Financial Metric ("+finmetric+")"
-		subtitle = ""
+		subtitle = "Rs Cr (Except %); Source - TRAI"
 
 
 		
