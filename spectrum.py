d5f18887580e5f51db8d91edb26de8b60adbb745
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-20 09:33:25 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1560,9 +1560,7 @@ if authentication_status:
 
 	for index in dfrsrate.index:
 
-		st.write(index.date())
-
-		if index in auction_eom_list:
+		if index.date() in auction_eom_list:
 			st.write(True)
 
 	
