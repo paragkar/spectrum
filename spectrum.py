3353b208c1078cfa47ab4cf00d9b709d9d2c0d7d
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-20 15:00:33 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2001,7 +2001,7 @@ if authentication_status:
 					for line in typedf.values:
 						for i, val in enumerate(line):
 							if str(val).strip() != "NA":
-								st.write(val,[curr_list[i]][0])
+								st.write(val/[curr_list[i]][0])
 						
 			
 			
