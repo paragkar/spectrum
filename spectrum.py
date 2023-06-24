4633944623c6b41cc5edf054e9f5943d6a080c01
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-24 10:35:58 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -3333,9 +3333,9 @@ if authentication_status:
 			#------------------New Code ---------------------#
 
 
-			dfbid = loadauctionbiddata()[demandsheet].replace('-', np.nan, regex = True)
+			# dfbid = loadauctionbiddata()[demandsheet].replace('-', np.nan, regex = True)
 
-			st.write(dfbid)
+			# st.write(dfbid)
 
 
 			# dfbidaBlksSale = dfbid.pivot(index="LSA", columns='Clock Round', values="Blocks For Sale").sort_index(ascending=True)
