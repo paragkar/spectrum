b129737db68d81ab548f99ec2d5227a839085858
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-07-04 06:58:55 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -5706,7 +5706,7 @@ if authentication_status:
 
 				hovertext = htext_businessdata_5gbts(df5gbtsf)
 
-				summarydf = df5gbtsf.sum(axis=0)
+				summarydf = df5gbtsfall.sum(axis=0) #debug
 
 				df5gbtsfPercent = round((df5gbtsf/summarydf)*100,2)
 
