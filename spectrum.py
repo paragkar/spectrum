31723627583a67d72b56aa9ab54aeb77f10d0ed6
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-03-12 09:25:27 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -4956,6 +4956,8 @@ if selected_dimension == "Auction Data":
 		# Note: The actual round iteration will happen elsewhere, so it doesn't block rendering.
 		round_number = st.session_state.current_round
 
+		st.write(round_number)
+
 		# round_number = st.slider("Select Auction Round Numbers using the Silder below", min_value=1, max_value=totalrounds, step=1, value = totalrounds)
 
 		#--------------------New Slider Logic Ends------------------#
