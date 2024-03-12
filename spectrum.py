a629e33b2b27a34da80f929e03d936c986dec1ff
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-03-12 09:07:15 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -4955,8 +4955,8 @@ if selected_dimension == "Auction Data":
 		else:
 		    # Manual control
 		    st.session_state.current_round = st.slider("Select Auction Round Numbers using the Slider below", min_value=1, max_value=totalrounds, step=1, value=st.session_state.current_round)
-
-	    round_number = st.session_state.current_round
+		    
+		round_number = st.session_state.current_round
 
 		# round_number = st.slider("Select Auction Round Numbers using the Silder below", min_value=1, max_value=totalrounds, step=1, value = totalrounds)
 
