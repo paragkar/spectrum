27bfb1d29c13e3461a9ac36807d55dc77ead63f6
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-03-30 19:22:26 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -7234,10 +7234,10 @@ if selected_dimension in ["Spectrum Bands", "Auction Years", "Business Data"]:
 
 
 		#processing chart for total of all columns 
-		summarydf = summarydf.T.reset_index()
-		summarydf.columns =[timescale, dimension]
+		# summarydf = summarydf.T.reset_index()
+		# summarydf.columns =[timescale, dimension]
 		bar_data = summarychart(summarydf, timescale, dimension)
-		fig2 = go.Figure(data=chart)
+		fig2 = go.Figure(data=bar_data)
 
 		if chart_data_flag==True:
 			tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"]) #for listing the summary chart for freq layout
