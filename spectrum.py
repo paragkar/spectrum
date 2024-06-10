26dfc3a247d0050be03bb87ba0b7a2e06e78ab12
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-10 13:19:45 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -4035,7 +4035,7 @@ if selected_dimension == "Auction Data":
 		  template='simple_white',
 		  paper_bgcolor=None,
 		  height=600, 
-		  # width=1200,
+		  width=1200,
 		  margin=dict(t=80, b=50, l=50, r=50, pad=0),
 		  yaxis=dict(
         	  tickmode='array'),
