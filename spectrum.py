f4ad64a72bf5a80ec1c8ad5d14065ec194f5144f
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-12 09:08:07 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -3888,7 +3888,7 @@ if selected_dimension == "Auction Data":
 				dfbidblksec.replace("placeholder", np.nan, inplace=True)
 
 
-				st.write(dfbidblksec.values)
+				st.write(dfbidblksec.dtypes)
 
 				#-----------End---------------------
 
