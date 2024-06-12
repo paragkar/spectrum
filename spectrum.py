b04fc3e2d80b47a2005272f32796cc4162a31f8f
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-12 09:11:35 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -3878,17 +3878,10 @@ if selected_dimension == "Auction Data":
 				dfbidblksec = dfbidblksec.sort_index(ascending=True)
 
 				
-				dfbidblksec = dfbidblksec.pivot_table(index='LSA', columns='Bidder', values='No_of_BLK_Selected', aggfunc=lambda x: list(x)).T
+				dfbidblksec = dfbidblksec.pivot_table(index='LSA', columns='Bidder', values='No_of_BLK_Selected', aggfunc=lambda x: list(str(x)).T
 
 
-				dfbidblksec.replace("", np.nan, inplace=True)
-
-				dfbidblksec.replace(0, np.nan, inplace=True)
-
-				dfbidblksec.replace("placeholder", np.nan, inplace=True)
-
-
-				st.write(dfbidblksec.values[0])
+				st.write(dfbidblksec)
 
 				#-----------End---------------------
 
