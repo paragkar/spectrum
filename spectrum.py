b62c7e39dde235673e229680d39f225e2d8cf28a
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-12 08:28:50 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -3875,7 +3875,7 @@ if selected_dimension == "Auction Data":
 
 				dfbidblksec = dfbidblksec.sort_index(ascending=True)
 
-				st.write(dftemp1)
+				st.write(dfbidblksec)
 
 				#-----------End---------------------
 
