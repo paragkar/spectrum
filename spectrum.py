7dc7a9ae31a7d5cab5d9f890d70f30c0271f96bb
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-15 12:35:11 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1870,7 +1870,7 @@ def plotbiddertotal(dftemp,dfblocksalloc_rdend):
 
 	panindiabids.columns =["Bidder","PanIndiaBid"]
 	panindiabids = panindiabids.round(0)
-	panindiabids = panindiabids.sort_values("Bidder", ascending=False)
+	panindiabids = panindiabids.sort_values("Bidder", ascending=True)
 
 	st.write(panindiabids)
 
