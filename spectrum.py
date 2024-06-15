fb74c759937d1bad61df508ffd51a4268654b0fd
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-15 10:59:29 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1570,7 +1570,7 @@ if selected_dimension == "Spectrum Bands":
 
 			#Debug 14th June 2024 -----Start
 
-			sumrows = dfff.sum(axis=1).reset_index(drop=True)
+			sumrows = dfff.sum(axis=1).reset_index()
 
 			st.write(sumrows)
 			# figsumrows = plotrwototal(sumrows,"Operators", "Total MHz")
