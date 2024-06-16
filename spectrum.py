3e44447a6e6e1e700b4d1a5b18ea7a33cc0979b7
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-16 10:20:18 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1335,7 +1335,7 @@ def plotbiddertotal(dftemp,dfblocksalloc_rdend):
 	fig.update_layout(xaxis=dict(title='Total Value'), yaxis=dict(title=''))
 	fig.update_traces(text=panindiabids['PanIndiaBid'], textposition='auto',textfont=dict(size=text_embed_in_chart_size, color='white')) #Debug 12th June 2024 (Changed 14 to 20)
 	fig.update_xaxes(tickvals=[])
-	fig.update_layout(xaxis=dict(side='top', title_standoff=0, ticklen=0, title_font=dict(size=14)))
+	fig.update_layout(xaxis=dict(side='top', title_standoff=0, ticklen=0, title_font=dict(size=text_embed_in_chart_size)))
 	fig.update_layout(xaxis_title_standoff=5)
 	fig.update_traces(marker=dict(color='red'))
 
