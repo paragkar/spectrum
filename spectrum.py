80235e4156eba1a7eb8bc8edcb3218ca10ccb28a
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-16 08:41:51 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -3861,7 +3861,7 @@ if selected_dimension == "Auction Data":
 		col1,col2 = st.columns([stcol1,stcol2]) #create collumns of uneven width
 		col1.plotly_chart(figauc, use_container_width=True)
 		space = col1.empty()
-		space.text(" ")
+		space.text("  ")
 		col1.altair_chart(figsumcols, use_container_width=True)
 		col2.markdown("")
 		col2.plotly_chart(figsumrows, use_container_width=True)
