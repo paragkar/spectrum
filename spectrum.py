16979c615b3f0cd27b8dc156d6899f6b7fe8211c
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-16 09:04:55 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1262,7 +1262,7 @@ def transform_colscale_for_hbox_auction_map(dff,reserveprice, auctionprice):
 def summarychart(summarydf, xcolumn, ycolumn):
 	bar = alt.Chart(summarydf).mark_bar().encode(
 	y = alt.Y(ycolumn+':Q', axis=alt.Axis(labels=True, titleAngle =0, titleFontSize=text_embed_in_chart_size,labelAngle=0,labelFontSize=text_embed_in_chart_size)),
-	x = alt.X(xcolumn+':O', axis=alt.Axis(labels=True, labelAngle=0, labelFontSize=text_embed_in_chart_size)), #Debug 12th June 2024 (Changed False to True)
+	x = alt.X(xcolumn+':O', axis=alt.Axis(labels=True, labelAngle=0, labelFontSize=text_embed_in_chart_size, titleFontSize=text_embed_in_chart_size)), #Debug 12th June 2024 (Changed False to True)
 	color = alt.Color(xcolumn+':N', legend=None))
 
 	text = bar.mark_text(size = text_embed_in_chart_size, dx=0, dy=-7, color = 'white').encode(text=ycolumn+':Q') #Debug 12th June 2024 (Chnaged Bar Text Sixe from 10 to 16)
