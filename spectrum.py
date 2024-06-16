d716d70573483af03c2492f4a71bd6154e77c3b9
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-16 06:36:50 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1276,7 +1276,7 @@ def plotrwototal(sumrows, ydim, xdim):
 	fig.update_xaxes(tickvals=[])
 	fig.update_layout(xaxis=dict(side='top', title_standoff=0, ticklen=0, title_font=dict(size=text_embed_in_chart_size))) #Debug 12th June 2024 (Changed 14 to 20)
 	# Update y-axis label size
-	fig.update_yaxes(title_font=dict(size=16))  # Change '16' to your desired font size for y-axis labels
+	fig.update_yaxes(title_font=dict(size=28))  # Change '16' to your desired font size for y-axis labels
 	fig.update_layout(xaxis_title_standoff=0) 
 	fig.update_traces(marker=dict(color='red'))
 	return fig
