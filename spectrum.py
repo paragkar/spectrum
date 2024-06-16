e50695eecbcba329778c79aa8a96eb2e853492fd
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-16 18:55:14 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2486,7 +2486,7 @@ if selected_dimension == "Auction BandWise":
 	Feature = st.sidebar.selectbox("Select a Feature", year_band) #Debug 10th June 2024
 
 	if Feature in features:
-		st.write(features[Feature])
+		# st.write(features[Feature])
 		st.write(features[Feature][totalrounds])
 
 	# if Feature == "2022-Band26000":
