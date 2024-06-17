df956f131d701e9e6044ec0be0c690408e42a609
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-17 13:49:14 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1946,7 +1946,7 @@ if selected_dimension == "Auction Integrated":
 	for sepectrumband in year_band:
 		sheet = Auction_Year_Band_Features[sepectrumband]["demandsheet"]
 
-		st.write(loadauctionbiddata()[sheet])
+		st.write(sheet,loadauctionbiddata()[sheet])
 
 
 if selected_dimension == "Spectrum Bands":
