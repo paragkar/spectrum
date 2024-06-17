f344f77599233969abdcedba4fc4baa83efe8721
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-17 10:56:57 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -3692,10 +3692,7 @@ if selected_dimension == "Auction BandWise":
 
 			st.plotly_chart(figauc, use_container_width=True)
 
-
-
-    # if SelectedSubFeature == "ProvWinningBid":
-        
+    if SelectedSubFeature == "ProvWinningBid":
         dfbid1 = loadauctionbiddata()[demandsheet].replace('-', np.nan,regex=True)
         dfbid1 = dfbid1.drop(columns=["Clock Round", "Clock Round Price (Rs. Crore)", "Aggregate Demand", "Excess Demand"], axis=1)
         dfbid1 = dfbid1.groupby(["LSA"]).mean().reset_index()
