9986f69068140fb99c9cae0bfdc4d4aa869aecd7
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-17 10:08:01 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -3100,6 +3100,9 @@ if selected_dimension == "Auction BandWise":
 		#------------------New Code Starts---------------------#
 		dfbid1 = loadauctionbiddata()[demandsheet].replace('-', np.nan, regex = True)
 		dfbid1 = dfbid1.drop(columns =["Clock Round", "Clock Round Price (Rs. Crore)", "Aggregate Demand", "Excess Demand"], axis =1)
+
+        st.write(dfbid1)
+        
 		dfbid1 = dfbid1.groupby(["LSA"]).mean().reset_index()
 		dfbid1.columns = ["LSA", "BlocksForSale"]
 		summarydf = dfbid1.copy()
