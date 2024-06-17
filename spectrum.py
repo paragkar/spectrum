618b441517575327309cff5f49ee8dabdc8a983c
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-17 11:08:15 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -3705,8 +3705,8 @@ if selected_dimension == "Auction BandWise":
 		#-----------------New Code Ends ----------------------#
 
 		pwbtype = st.sidebar.selectbox("Select a PWB Type", ["Start CLK Round", "End CLK Round"])
-		# df1strd = dfbid1[dfbid1["Clk_Round"] == 1].reset_index()  # Identifying the 2nd round gives up the reserve price
-		df1strd = dfbid1[dfbid1["Clock Round"] == 1].reset_index()  # Identifying the 2nd round gives up the reserve price
+		df1strd = dfbid1[dfbid1["Clk_Round"] == 1].reset_index()  # Identifying the 2nd round gives up the reserve price
+		# df1strd = dfbid1[dfbid1["Clock Round"] == 1].reset_index()  # Identifying the 2nd round gives up the reserve price
 		dfpwb1strdend = df1strd.pivot(index="Bidder", columns='LSA', values="PWB_End_ClkRd").sort_index(ascending=False)
 
 		dfrp = dfpwb1strdend.T  # Preparing the dataframe for reserve price per BLK
