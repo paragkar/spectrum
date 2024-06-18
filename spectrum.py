1bf08f77fe1ee0348da33e7709ac1fec87928393
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-18 18:50:54 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -5694,6 +5694,8 @@ if currency_flag == False: #USD
 					  "Auction/Reserve" : "Ratio", "Percent Unsold" : "% of Total Spectrum", "Percent Sold" : "% of Total Spectrum", 
 					  "Total Purchase" : "MHz"}
 
+
+#-------This is a new Code---- for Auction Integrated Structure--Remove this if this does not work and change the indentation below 
 if currency_flag == "NA": #This option is for Auction Integrated so as ensure the current structure is preserved
 	pass
 else:
