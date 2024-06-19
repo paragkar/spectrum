129c59127ff41dee975c7d09b34bea5414534515
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-19 18:13:56 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2013,7 +2013,7 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 	# Extract band information more reliably
 	# df['Band'] = df.index.str.extract(r'(\d+)')[0]
 
-	st.write(df.index.str.extract(r'(\d+)'))
+	df["Band"] = df.index.str.extract(r'(\d+)')
 
 	# # Dictionary to hold dataframes for each band
 	# df_dict = {band: group.drop('Band', axis=1) for band, group in df.groupby('Band')}
