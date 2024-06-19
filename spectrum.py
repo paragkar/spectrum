b8a5d120c96320724e7a0ac95d18258893bf23e6
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-19 15:19:04 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1968,7 +1968,7 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 
 	df = loadauctionbiddatayearbandcomb()
 
-	st.write(df)
+	st.write(df.columns)
 
 # 	#columns names are used to rename the loaded dataframe
 # 	column_names = ["Clock Round", "Bidder", "Service Area", "Prov WinBid Start Rd","Rank Start Rd","Can BidPrice Increase Y/N","Bid decision","Prov WinBid End Rd",	
