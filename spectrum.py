13dada0c5ee2be0b4ae88c017b037e877cc3872e
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-19 07:21:09 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1954,13 +1954,13 @@ if selected_dimension == "Auction Integrated":
 	# dfcomb.columns = column_names
 	for sepectrumband in year_band:
 		sheet = Auction_Year_Band_Features[sepectrumband]["mainsheet"]
+		df = pd.DataFrame()
 		try:
 			band = sheet.split("_")[2]
 			auctionyear = sheet.split("_")[0]
 			df.columns = column_names
-
 			df = loadauctionbiddata()[sheet]
-			# st.write(df)
+
 			df["Auction Year"] = auctionyear
 			df["Band"] = band
 
