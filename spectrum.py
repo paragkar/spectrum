c9a59b2b024f025854a3a45cdd58c6aae341920e
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-19 09:32:26 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1990,7 +1990,7 @@ if selected_dimension == "Auction Integrated":
 	# Simplify column names for display
 	column_labels = [f"{col[0]} ({col[1]})" for col in dfcomb_aucyr_dim_rd.columns]
 
-	dfcomb_aucyr_dim_rd = dfcomb_aucyr_dim_rd.fillna(0)
+	dfcomb_aucyr_dim_rd = dfcomb_aucyr_dim_rd.fillna("")
 
 	# Prepare text to embed in the heatmap itself
 	text_values = [[f"{value}" if pd.notna(value) else "" for value in row] for row in dfcomb_aucyr_dim_rd.values]
