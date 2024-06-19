7c03a7b9e9bf2a3443dee092b849364e2c76280f
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-19 18:04:48 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2013,10 +2013,12 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 	# Extract band information
 	df['Band'] = df.index.str.extract(r'(\d+)')[0]
 
+	st.write(df)
+
 	# Dictionary to hold dataframes for each band
 	df_dict = {band: group.drop('Band', axis=1) for band, group in df.groupby('Band')}
 
-	st.write(df_dict)
+	# st.write(df_dict)
 
 
 	# # Create the heatmap object
