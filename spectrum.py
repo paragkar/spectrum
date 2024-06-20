d7c8a7597c1da38fbcc4363aa3c21f4511633b21
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-20 15:01:20 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2056,8 +2056,8 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 		text_values = df_segment.replace(np.nan, '')
 		aligned_color_df = color_df.loc[df_segment.index, df_segment.columns].replace(np.nan, 0)
 
-		st.write(aligned_color_df)
-		st.write(colorscale)
+		# st.write(aligned_color_df)
+		# st.write(colorscale)
 
 		# Create a heatmap for each band
 		fig.add_trace(
