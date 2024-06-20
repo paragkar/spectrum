55d446a920b8ef0425e8c3885763db5d01859c73
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-20 09:16:11 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2038,9 +2038,10 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 			row=i, col=1
 		)
 
-		# Update axes to their original settings
-		fig.update_xaxes(row=i, col=1, fixedrange=True, showline=True, linewidth=1.2, linecolor='black', mirror=True, showgrid=True, gridcolor='lightgrey')
-		fig.update_yaxes(row=i, col=1, fixedrange=True, showline=True, linewidth=1.2, linecolor='black', mirror=True, showgrid=True, gridcolor='lightgrey')
+		# Update axes to ensure y-axis labels are always visible
+		fig.update_xaxes(row=i, col=1, fixedrange=True, showline=True, linewidth=1.2, linecolor='black', mirror=True, showgrid=True, gridcolor='lightgrey', tickfont=dict(size=text_embed_in_chart_size))
+		fig.update_yaxes(row=i, col=1, fixedrange=True, showline=True, linewidth=1.2, linecolor='black', mirror=True, showgrid=True, gridcolor='lightgrey', tickmode='array', tickvals=list(range(len(df.index))), ticktext=df.index, tickfont=dict(size=text_embed_in_chart_size))
+
 
 		# Update axes for each subplot to set the tick font size
 		fig.update_xaxes(row=i, col=1, tickfont=dict(size=text_embed_in_chart_size))
