cc11675b5c0f63ac343823f2d41dfcb57bf4c572
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-20 09:08:00 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2039,8 +2039,8 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 		)
 
 		# Update axes to their original settings
-		fig.update_xaxes(row=i, col=1, fixedrange=True, showline=True, linewidth=1.2, linecolor='black', mirror=True, showgrid=True, gridcolor='lightgray')
-		fig.update_yaxes(row=i, col=1, fixedrange=True, showline=True, linewidth=1.2, linecolor='black', mirror=True, showgrid=True, gridcolor='lightgray')
+		fig.update_xaxes(row=i, col=1, fixedrange=True, showline=True, linewidth=1.2, linecolor='black', mirror=True, showgrid=True, gridcolor='white')
+		fig.update_yaxes(row=i, col=1, fixedrange=True, showline=True, linewidth=1.2, linecolor='black', mirror=True, showgrid=True, gridcolor='white')
 
 		# Update axes for each subplot to set the tick font size
 		fig.update_xaxes(row=i, col=1, tickfont=dict(size=text_embed_in_chart_size))
