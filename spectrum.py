c13c9558dbbeaed216762ac29ccd171c72d8ab6b
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-20 09:09:10 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2060,7 +2060,7 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 		width=heatmapwidth,
 		height=heatmapheight * len(df_dict)*0.3,  # Total height based on the number of subplots
 		autosize=True,
-		plot_bgcolor='#87CEEB',  # Background color for the plot area light greay
+		plot_bgcolor='white',  # Background color for the plot area light greay
 		paper_bgcolor='white',
 		margin=dict(t=30, b=30, l=30, r=30, pad=4),
 		yaxis=dict(
