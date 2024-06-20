50ac8734a3ee98129a5d780aab80ffa398532f60
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-20 15:05:31 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2070,7 +2070,7 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 				texttemplate="%{text}",
 				textfont={"size": text_embed_in_chart_size}, 
 				showscale=False,
-				reversescale=False,
+				reversescale=True,
 			),
 			row=i, col=1
 		)
