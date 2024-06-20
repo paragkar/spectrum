7ab401cbdc2566a92cbd0003b5319a87324ce50d
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-20 10:21:23 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2056,7 +2056,7 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 						title_text="" if has_non_zero_values else str(band))  # Set the y-axis title here)
 
 	
-	height_mul_dict = {2022:0.17, 2021: 0.2, 2016:0.205, 2015 : 0.32}
+	height_mul_dict = {2022:0.17, 2021: 0.2, 2016:0.205, 2015 : 0.32, 2014 : 0.32}
 
 	# Update the overall layout
 	fig.update_layout(uniformtext_minsize=text_embed_in_chart_size, 
