a267174d628ee65b62914a866bbfd83785c0d7cc
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-20 10:46:30 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2007,7 +2007,11 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 
 	df = df.replace("", np.nan)
 
-	# df = df.dropna(axis=0, how='all')  # Ensure rows with all NaN are dropped
+	# Extract bidder names from column labels and map them to a unique integer
+	unique_bidders = {col.split(' ')[1]: idx for idx, col in enumerate(df.columns.unique())}
+	# Create a new column for color mapping based on the bidder
+	df['Bidder_Color'] = df.apply(lambda x: unique_bidders[x.name.split(' ')[1]], axis=1)
+
 
 	# Define a colorscale for the heatmap
 	colorscale = "Hot"  # or any other color scale available in Plotly
