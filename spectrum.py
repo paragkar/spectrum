5987d5d47814df5803836f658bd818446fffb800
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-20 11:44:35 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2061,7 +2061,7 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 				x=df_segment.columns,
 				y=df_segment.index,
 				text = df_segment.values,
-				colorscale='Hot',  # You can change the color scale as needed
+				# colorscale='Hot',  # You can change the color scale as needed
 				texttemplate="%{text:.0f}",
 				textfont={"size": text_embed_in_chart_size}, 
 				showscale=False,
