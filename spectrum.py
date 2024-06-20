511efb745057fcedc785863dbe4de4234eb16cbd
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-20 10:09:00 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2068,7 +2068,7 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 		template='simple_white',
 		# title='Heatmap of No. of Blocks Selected by Service Area and Band',
 		width=heatmapwidth,
-		height=heatmapheight * len(non_empty_dfs)*0.18,  # Total height based on the number of subplots
+		height=heatmapheight * len(non_empty_dfs)*0.17,  # Total height based on the number of subplots
 		autosize=True,
 		plot_bgcolor='lightgrey',  # Background color for the plot area light greay
 		paper_bgcolor='white',
