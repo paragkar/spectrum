97a4eca90e0e47ac97cec1056c802f11c7fc5b6d
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-21 12:25:44 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2171,7 +2171,7 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 
 	bands_in_view = len(df_dict.keys())
 
-	height_mul_dict = {1:0.8, 2:1, 3: 1.2, 4 :1.25 , 5 :1.35, 6 : 1.45 , 7: 1.45, 8: 1.45, 9:1.45}
+	height_mul_dict = {1:0.8, 2:1, 3: 1.2, 4 :1.2 , 5 :1.35, 6 : 1.45 , 7: 1.45, 8: 1.45, 9:1.45}
 
 
 
