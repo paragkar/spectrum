d4d7b8f7e9ebcba60241ab014aec509170949deb
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-21 08:49:45 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2100,9 +2100,9 @@ if selected_dimension == "Auction Integrated": #This is the new dimension that i
 					texttemplate="%{z:.1f}",
 					textfont={"size": text_embed_in_chart_size*0.8}, 
 					showscale=False,
-					reversescale=True,
-					zmin=zmin,  # Set minimum z value
-					zmax=zmax,  # Set maximum z value
+					# reversescale=True,
+					# zmin=zmin,  # Set minimum z value
+					# zmax=zmax,  # Set maximum z value
 				),
 				row=i, col=1
 			)
