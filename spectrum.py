b1ca55412b8f81ab4e0edc592d96bfbc8493ce6d
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-23 16:05:13 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2270,9 +2270,9 @@ if selected_dimension == "AuctionYear AllBands": #This is the new dimension Adde
 
 	def text_values_heatmap(selected_dimension, df_segment, band):
 	    if selected_dimension in  lambda_function_dict:
-	        text_values = lambda_function_dict[selected_dimension](band).replace('nan', '')
+	        text_values = lambda_function_dict[selected_dimension](band)
 	    else:
-	        text_values = df_segment.astype(float).round(0).astype(str).replace('nan', '')
+	        text_values = df_segment.astype(float).round(0).astype(str)
 	    return text_values
 
 
