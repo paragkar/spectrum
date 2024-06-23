72a8ae47c4c9eb90e17a6fe5db18fd6ebf8fce4e
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-23 19:07:04 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2284,7 +2284,7 @@ if selected_dimension == "AuctionYear AllBands": #This is the new dimension Adde
 	        texttemplate ="%{text}"
 	    else:
 	        text_values = df_segment.astype(float).round(0).astype(str).replace('nan',"")
-	        texttemplate ="%{text:.1f}"
+	        texttemplate ="%{text:.2f}"
 	    return text_values, texttemplate
 
 
