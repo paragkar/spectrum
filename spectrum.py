876ca92e00d7936475a152023842ddcfeb53e590
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-23 13:32:01 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2232,7 +2232,7 @@ if selected_dimension == "AuctionYear AllBands": #This is the new dimension Adde
 
 	def prepare_text_values(df_dict, result_df_dict, band):
 		# df = df_dict[band].astype(float).round(0).astype(str).replace('0', '').replace('nan', '')
-		df = df_dict[band].astype(str)
+		df = df_dict[band].map(lambda x : round(x,0)).astype(str).replace('nan', '')
 		result_df = result_df_dict[band].astype(str)
 		combined_df = df + '\n' + result_df
 		return combined_df
