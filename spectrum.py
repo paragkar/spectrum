0fb8c7b22fa2ca02d3a3553d1f831dd836c74cf8
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-23 16:13:52 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2272,7 +2272,7 @@ if selected_dimension == "AuctionYear AllBands": #This is the new dimension Adde
 	    if selected_dimension in  lambda_function_dict:
 	        text_values = lambda_function_dict[selected_dimension](band)
 	    else:
-	        text_values = df_segment.astype(float).round(0).astype(str)
+	        text_values = df_segment.astype(float).round(0).astype(str).replace('nan',"")
 	    return text_values
 
 
