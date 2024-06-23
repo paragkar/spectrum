9a180a82ddb0e2ee7d9241e723ee1b1393a8c933
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-23 18:24:52 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2286,6 +2286,8 @@ if selected_dimension == "AuctionYear AllBands": #This is the new dimension Adde
 
 			text_values, texttemplate = text_values_heatmap(selected_dimension,df_segment,band)
 
+			st.write(text_values.dtypes)
+
 
 			aligned_color_df = color_df.loc[df_segment.index, df_segment.columns].replace(np.nan, "")
 			# Create a heatmap for each band
