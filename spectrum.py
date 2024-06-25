75675798201ac3cce585cd9b4114b0ef5a9ee5b9
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-25 08:20:21 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2105,7 +2105,7 @@ if selected_dimension == "AuctionYear AllBands": #This is the new dimension Adde
 	dfcopy = filt_round(dfcopy, st.session_state.round_number)
 	dfactvity = filt_round(dfactvity, st.session_state.round_number)
 
-	activity_factor_for_selected_round = dfactvity.round(2).drop_duplicates()["Activity Factor"].values #Note this will be used in the title 
+	activity_factor_for_selected_round = dfactvity.drop_duplicates()[["Activity Factor"]]#Note this will be used in the title 
 
 	# Function to Pivot Dataframe based on selected dimention
 	def pivot_dataframe(df, selected_dimension):
