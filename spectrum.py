51cce1809a2a83073e4c61ed57663713c076ab0d
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-28 06:43:50 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -2903,7 +2903,7 @@ if selected_dimension == "Spectrum Bands":
 			hovertext = htext_expmap_freq_layout(hf)
 
 			#Selecting text to render on heatmap depend on band
-			if Band in [1800, 26000, 3500]:
+			if Band in [900, 1800, 26000, 3500]:
 				text = ""
 			else:
 				text = "%{z}"
