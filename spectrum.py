36e4c67ea72d8b286678fdf2f2aabad982ca06e3
Parag Kar <40283772+paragkar@users.noreply.github.com>
2025-06-23 10:05:20 +0530

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -117,7 +117,7 @@ hide_st_style = '''
 	header {visibility: hidden;}
 	</style>
 	'''
-st.markdown(hide_st_style, unsafe_allow_html=True)
+# st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # hide_st_style = '''
 # 				<style>
